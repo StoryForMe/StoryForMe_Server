605629634db32b17b02050659beba5ef1980c9f9
SeoSiun <siun0816@hanyang.ac.kr>
2022-04-26 14:01:02 +0900

    fix: keyword sql

--- routes/keyword.js
+++ routes/keyword.js
@@ -2,7 +2,7 @@ const app = require('../app');
 
 exports.getSeriesKeyWord = (sid) => {
 	app.getConnectionPool((conn) => {
-		var sql = "select * from KEYWORD where sid=" + sid;
+		var sql = "select content from KEYWORD as k join REPRESENT as r on k.id=r.kid where sid=" + sid;
 		conn.query(sql, function(err, keywords) {
 			if(err) console.log("getSeriesKeyWord err");
 			else return (keywords);
