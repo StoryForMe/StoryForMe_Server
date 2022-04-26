2179b51386f3f99c2be3f6ef7ffbd7119cfc0b8c
SeoSiun <siun0816@hanyang.ac.kr>
2022-04-26 12:25:11 +0900

    fix: not a function error

--- routes/series.js
+++ routes/series.js
@@ -1,26 +1,9 @@
 var express = require('express');
 var router = express.Router();
-const getConnectionPool = require('../app');
-
-// const mysql = require('mysql');
-// const connectionPool = mysql.createPool({
-// 	host: process.env.DB_HOST,
-// 	user: process.env.DB_USER,
-// 	password: process.env.DB_PW,
-// 	port: process.env.DB_PORT,
-// 	database: process.env.DB_NAME,
-// 	insecureAuth: true,
-// });
-
-// function getConnectionPool(callback) {
-// 	connectionPool.getConnection((err, conn) => {
-// 		if(!err) callback(conn);
-// 		else console.log("err");
-// 	})
-// }
+const app = require('../app');
 
 router.get('/:id', (req, res) => {
-	getConnectionPool((conn) => {
+	app.getConnectionPool((conn) => {
 		var sql = "select * from series where id=" + id;
 		conn.query(sql, function(err, rows) {
 			res.json(rows);
