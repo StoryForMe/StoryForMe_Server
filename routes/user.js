3a730bb929cd7e55dffd368cadff27987017824c
SeoSiun <55352430+SeoSiun@users.noreply.github.com>
2022-04-27 17:52:15 +0900

    utils (#11)
    get, post 등에서 사용하는 함수들을 utils 폴더에 정리

--- routes/user.js
+++ routes/user.js
@@ -23,15 +23,4 @@ router.get('/:login/:email', (req, res) => {
   })
 })
 
-exports.getNickname = (uid, callback) => {
-	return app.getConnectionPool((conn) => {
-		var sql = "select nickname from USER where id=" + uid;
-		conn.query(sql, function(err, [row]) {
-			conn.release();
-			if(err) console.log("getNickname err");
-			else callback(row["nickname"]);
-		})
-	})
-}
-
 module.exports = router;
