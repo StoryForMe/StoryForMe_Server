9c0cfbc7118711c011e9cccd45c57a37384d9cb7
rudolf0328 <ck0716080@gmail.com>
2022-04-27 15:28:29 +0900

    try

--- routes/user.js
+++ routes/user.js
@@ -1,4 +1,26 @@
+const router = require('.');
 const app = require('../app');
+
+router.get('/:login-method/:email', (req, res) => {
+  app.getConnectionPool((conn) => {
+    var sql = "select * from USER where login_method=" + req.params.login-method + "and email=" + req.params.email;
+    conn.query(sql, function(err, user) {
+      conn.release();
+      if(err) console.log("error");
+      else if(!user) {
+        var result = {
+          id: -1
+        }
+        res.json(result);
+      } else {
+        var result = {
+          id: user["id"]
+        }
+        res.json(result);
+      }
+    })
+  })
+})
 
 exports.getNickname = (uid, callback) => {
 	return app.getConnectionPool((conn) => {
