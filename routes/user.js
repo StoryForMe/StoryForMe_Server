c472ee35c74d5de2950dee7c342f01ab53ce2d4e
rudolf0328 <ck0716080@gmail.com>
2022-04-27 16:11:50 +0900

    d

--- routes/user.js
+++ routes/user.js
@@ -2,11 +2,9 @@ var express = require('express');
 var router = express.Router();
 const app = require('../app');
 
-router.get('/:email', (req, res) => {
-  console.log("ddd");
+router.get('/:login/:email', (req, res) => {
   app.getConnectionPool((conn) => {
-    console.log(req.params.email);
-    var sql = "select * from USER where email=" + req.params.email;
+    var sql = "select * from USER where login_method=" + req.params.login + "and email=" + req.params.email;
     console.log(sql);
     conn.query(sql, function(err, user) {
       conn.release();
