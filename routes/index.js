8d8a99cb73557911349c340effd215e08b0491e7
Rudolf0328 <54528011+Rudolf0328@users.noreply.github.com>
2022-04-27 16:34:56 +0900

    5 userlogin (#9)
    - login method를 이용해서 user id 전달
    - 나중에 ? 로 바꿔야지
    - user 있으면 id 주고 없으면 -1 전달
    - error 처리 http 상태 코드로 해야되는데..

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,12 @@
 var express = require('express');
 var router = express.Router();
 
-//const User = require('./user');
+const User = require('./user');
 const Series = require('./series');
 //const Episode = require('./episode');
 //const Comment = require('./comment');
 
-//router.use('/user', User);
+router.use('/user', User);
 router.use('/series', Series);
 //router.use('/episode', Episode);
 //router.use('/comment', Comment);
--- routes/user.js
+++ routes/user.js
@@ -1,4 +1,27 @@
+var express = require('express');
+var router = express.Router();
 const app = require('../app');
+
+router.get('/:login/:email', (req, res) => {
+  app.getConnectionPool((conn) => {
+    var sql = "select * from USER where login_method=" + req.params.login + " and email='" + req.params.email +"'";
+    conn.query(sql, function(err, [user]) {
+      conn.release();
+      if(err) console.log("error");
+      else if(!user) {
+        var result = {
+          id: -1
+        }
+        res.json(result);
+      } else {
+        var result = {
+          id: user["id"]
+        }
+        res.json(result);
+      }
+    })
+  })
+})
 
 exports.getNickname = (uid, callback) => {
 	return app.getConnectionPool((conn) => {
@@ -10,3 +33,5 @@ exports.getNickname = (uid, callback) => {
 		})
 	})
 }
+
+module.exports = router;
