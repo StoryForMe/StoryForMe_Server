a41b87b88a404c21cabd23dbed59e6a4c45353d8
Rudolf0328 <54528011+Rudolf0328@users.noreply.github.com>
2022-04-29 20:08:41 +0900

    13 user get character (#25)
    - user id를 받아서 character 이름을 반환하는 api
    - router가 코드 순서대로 확인
    - 구분 가능한 순서로 코드를 짜야됨

--- routes/user.js
+++ routes/user.js
@@ -3,12 +3,31 @@ var router = express.Router();
 const app = require('../app');
 const keyword = require('../utils/keyword');
 
+router.get('/:id/character', (req, res) => {
+  app.getConnectionPool((conn) => {
+    var sql = "select * from USER where id=" + req.params.id;
+    conn.query(sql, function(err, [user]) {
+      conn.release();
+      if(err) console.log("[USER] get character " + err);
+      else if(!user) {
+        console.log("no exist user.")
+      } else {
+        var result = {
+          fname: user["fname"],
+          lname: user["lname"]
+        }
+        res.json(result);
+      }
+    })
+  })
+})
+
 router.get('/:login/:email', (req, res) => {
   app.getConnectionPool((conn) => {
     var sql = "select * from USER where login_method=" + req.params.login + " and email='" + req.params.email +"'";
     conn.query(sql, function(err, [user]) {
       conn.release();
-      if(err) console.log("error");
+      if(err) console.log("[USER] login " + err);
       else if(!user) {
         var result = {
           id: -1
