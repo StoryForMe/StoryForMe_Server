0bb0335f71cb1c8a8d1b3572ca3f8be0910c8daa
SeoSiun <siun0816@hanyang.ac.kr>
2022-05-04 14:08:23 +0900

    .

--- routes/index.js
+++ routes/index.js
@@ -3,12 +3,12 @@ var router = express.Router();
 
 const User = require('./user');
 const Series = require('./series');
-//const Episode = require('./episode');
+const Episode = require('./episode');
 //const Comment = require('./comment');
 
 router.use('/user', User);
 router.use('/series', Series);
-//router.use('/episode', Episode);
+router.use('/episode', Episode);
 //router.use('/comment', Comment);
 
 router.get('/', function(req, res, next) {
