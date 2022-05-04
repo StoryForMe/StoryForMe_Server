1b21103cdee04d1148affe83d5109442233e7753
SeoSiun <siun0816@hanyang.ac.kr>
2022-05-04 14:06:09 +0900

    .

---
+++ routes/episode.js
@@ -0,0 +1,27 @@
+var express = require('express');
+var router = express.Router();
+const app = require('../app');
+
+router.post('/', (req,res) => {
+	app.getConnectionPool((conn) => {
+		var sql = "insert into EPISODE SET ?";
+		var values = {
+			sid: req.body.sid,
+			uid: req.body.uid,
+			title: req.body.title,
+			music: req.body.music,
+			content: req.body.content,
+			image: req.body.image,
+			state: req.body.state,
+			hits: 0,
+			date: new Date()
+		}
+		conn.query(sql, values, function(err, results) {
+			conn.release();
+			if(err) console.log(err);
+			else res.json({ result: 1 });
+		})
+	})
+})
+
+module.exports = router;
