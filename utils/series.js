32d28741427390cb601c522826c51b7879002790
SeoSiun <siun0816@hanyang.ac.kr>
2022-05-05 16:04:50 +0900

    .

--- utils/series.js
+++ utils/series.js
@@ -3,10 +3,10 @@ const app = require('../app');
 exports.getEpisodeNum = async (sid, callback) => {
 	return app.getConnectionPool((conn) => {
 		var sql = "select count(*) from EPISODE where sid=" + sid;
-		conn.query(sql, function(err, result) {
+		conn.query(sql, function(err, results) {
 			conn.release();
 			if(err) console.log(err);
-			else callback(result["count(*)"]); 
+			else callback(results[0]["count(*)"]); 
 		})
 	})
 }
