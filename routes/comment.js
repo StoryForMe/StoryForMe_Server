d2e09b5d08585c41e36c515afd54903b74c11cc4
SeoSiun <55352430+SeoSiun@users.noreply.github.com>
2022-05-16 19:43:41 +0900

    40 post comment (#41)
    [에피소드에 새로운 comment 추가하는 API]
    
    - 에피소드 primary key 바꾸면 얘도 바꿔야함.

---
+++ routes/comment.js
@@ -0,0 +1,23 @@
+var express = require('express');
+var router = express.Router();
+const app = require('../app');
+
+router.post('/', (req,res) => {
+	app.getConnectionPool((conn) => {
+		var sql = "insert into COMMENT SET ?";
+		var values = {
+			content: req.body.content,
+			uid: req.body.uid,
+			eid: req.body.eid,
+			esid: req.body.esid,
+			date: new Date()
+		}
+		conn.query(sql, values, function(err, results) {
+			conn.release();
+			if(err) console.log(err); 
+			else res.json({ result: 1 });
+		})	
+	})
+})
+
+module.exports = router;
