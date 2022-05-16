e86f62a2bf280f8c2d3dc534497454241867469e
SeoSiun <siun0816@hanyang.ac.kr>
2022-05-16 19:13:37 +0900

    .

--- routes/comment.js
+++ routes/comment.js
@@ -4,21 +4,19 @@ const app = require('../app');
 
 router.post('/', (req,res) => {
 	app.getConnectionPool((conn) => {
-		series.getEpisodeNum(req.body.sid, (episode_num) => {
-			var sql = "insert into COMMENT SET ?";
-			var values = {
-				content: req.body.content,
-				uid: req.body.uid,
-				eid: req.body.eid,
-				esid: req.body.esid,
-				date: new Date()
-			}
-			conn.query(sql, values, function(err, results) {
-				conn.release();
-				if(err) console.log(err); 
-				else res.json({ result: 1 });
-			})	
-		})
+		var sql = "insert into COMMENT SET ?";
+		var values = {
+			content: req.body.content,
+			uid: req.body.uid,
+			eid: req.body.eid,
+			esid: req.body.esid,
+			date: new Date()
+		}
+		conn.query(sql, values, function(err, results) {
+			conn.release();
+			if(err) console.log(err); 
+			else res.json({ result: 1 });
+		})	
 	})
 })
 
