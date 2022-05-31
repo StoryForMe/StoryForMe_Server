1e547234478f343fac904879cb41bbcc803600bb
SeoSiun <siun0816@hanyang.ac.kr>
2022-05-31 15:03:18 +0900

    .

--- utils/episode.js
+++ utils/episode.js
@@ -11,26 +11,25 @@ exports.getEpisodeList = (sid, callback) => {
 			else if (episodes.length == 0) callback([]);
 			else {
 				var result = [];
+				var index = 0;
 				// episode마다 comment 개수 가져오기. 
-				function getCommentNumCallback(comment_num, episode, next_index) {
+				function getCommentNumIterCallback(comment_num) {
 					var tmp = {
-						id : episode["id"],
-						title: episode["title"],
-						state: episode["state"],
+						id : episodes[index]["id"],
+						title: episodes[index]["title"],
+						state: episodes[index]["state"],
 						comment_num: comment_num,
-						date: episode["date"],
-						image: episode["image"],
-						hits: episode["hits"]
+						date: episodes[index]["date"],
+						image: episodes[index]["image"],
+						hits: episodes[index]["hits"]
 					}
 					result.push(tmp);
-					console.log("next_index: " + next_index);
-					console.log("episode_len: " + episodes.length)
 					// 마지막 episode면 callback함수 호출
 					if (next_index == episodes.length) callback(result);
 					// 마지막 episode가 아니면 다음 에피소드의 comment 개수를 가져옴.
-					else comment.getEpisodeCommentNum(episodes, next_index, getCommentNumCallback);
+					else comment.getEpisodeCommentNum(episodes[++index]["id"], getCommentNumIterCallback);
 				}
-				comment.getEpisodeCommentNum(episodes, 0, getCommentNumCallback);
+				comment.getEpisodeCommentNum(episodes[index]["id"], getCommentNumIterCallback);
 			}
 		})
 	})
