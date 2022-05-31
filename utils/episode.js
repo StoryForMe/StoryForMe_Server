c79271ade6788a98f0a5ed1c46096cc9ec5764e0
SeoSiun <siun0816@hanyang.ac.kr>
2022-05-31 14:56:23 +0900

    .

--- utils/episode.js
+++ utils/episode.js
@@ -23,7 +23,8 @@ exports.getEpisodeList = (sid, callback) => {
 						hits: episode["hits"]
 					}
 					result.push(tmp);
-
+					console.log(next_index);
+					console.log(episodes.length)
 					// 마지막 episode면 callback함수 호출
 					if (next_index == episodes.length) callback(result);
 					// 마지막 episode가 아니면 다음 에피소드의 comment 개수를 가져옴.
