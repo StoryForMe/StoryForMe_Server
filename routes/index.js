5dcbdfb16a116064c3859ec353952a613efacf84
SeoSiun <55352430+SeoSiun@users.noreply.github.com>
2022-05-31 17:07:50 +0900

    61 notice (#67)
    - [GET] notice
    - [GET] notice/:id
    - [POST] notice
    - [PUT] notice
    - [DELTE] notice

--- routes/index.js
+++ routes/index.js
@@ -5,12 +5,14 @@ const User = require('./user');
 const Series = require('./series');
 const Episode = require('./episode');
 const Comment = require('./comment');
+const Notice = require('./notice');
 const Zzimkkong = require('./zzimkkong');
 
 router.use('/user', User);
 router.use('/series', Series);
 router.use('/episode', Episode);
 router.use('/comment', Comment);
+router.use('/notice', Notice);
 router.use('/zzimkkong', Zzimkkong);
 
 router.get('/', function(req, res, next) {
