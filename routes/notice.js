7cad4b0314d0b432c73dbccbdc709f9feda3417d
SeoSiun <siun0816@hanyang.ac.kr>
2022-06-07 20:48:58 +0900

    .

--- routes/notice.js
+++ routes/notice.js
@@ -2,6 +2,25 @@ var express = require('express');
 var router = express.Router();
 const app = require('../app');
 
+function getNoticeData(nid, callback) {
+	app.getConnectionPool((conn) => {
+		var sql = "select * from NOTICE where id=" + nid;
+		conn.query(sql, function(err, notices) {
+			conn.release();
+			if (err) console.log(err);
+			else {
+				callback({
+					nid: notices[0]["id"],
+					title: notices[0]["title"],
+					content: notices[0]["content"],
+					hits: notices[0]["hits"],
+					date: notices[0]["date"]
+				})
+			}
+		})
+	})
+}
+
 router.get('/', (req, res) => {
 	app.getConnectionPool((conn) => {
 		var sql = "select * from NOTICE";
@@ -24,21 +43,8 @@ router.get('/', (req, res) => {
 	})
 })
 
-router.get('/:id', (req,res) => {
-	app.getConnectionPool((conn) => {
-		var sql = "select * from NOTICE where id=" + req.params.id;
-		conn.query(sql, function(err, notices) {
-			if (err) console.log(err);
-			else {
-				res.json({
-					title: notices[0]["title"],
-					content: notices[0]["content"],
-					hits: notices[0]["hits"],
-					date: notices[0]["date"]
-				})
-			}
-		})
-	})
+router.get('/:nid', (req,res) => {
+	getNoticeData(req.params.nid, (notice) => res.json(notice));
 })
 
 router.post('/', (req,res) => {
@@ -51,35 +57,38 @@ router.post('/', (req,res) => {
 			hits: 0
 		}
 		conn.query(sql, values, function(err, results) {
+			conn.release();
 			if(err) console.log(err); 
 			else {
-				// 방금 새로 만들어진 notice를 response로 보냄.
-				sql = "select * from NOTICE where id=" + results.insertId;
-				conn.query(sql, function(err, notices) {
-					conn.release();
-					if (err) console.log(err);
-					else {
-						res.json({
-							title: notices[0]["title"],
-							content: notices[0]["content"],
-							hits: notices[0]["hits"],
-							date: notices[0]["date"]
-						})
-					}
-				})
+				getNoticeData(results.insertId, (notice) => res.json(notice));
 			}
 		})	
 	})
 })
 
-router.put('/', (req, res) => {
+router.patch('/', (req, res) => {
+	if (req.body.id == undefined) {
+		res.json({ 
+			error: "E005",
+			error_message: "수정할 공지의 id 정보가 누락됨."
+		})
+	}
 	app.getConnectionPool((conn) => {
-		var sql = "update NOTICE SET content='" + req.body.content + "', title='" + req.body.title + "' where id=" + req.body.id;
-		conn.query(sql, function(err, results) {
+		var sql = "update NOTICE SET ? where id=" + req.body.id;
+		var values = {};
+		for(var key in req.body) {
+			if (key != "id") values[key] = req.body[key]
+		}
+		conn.query(sql, values, function(err, results) {
 			conn.release();
 			if (err) console.log(err);
-			else if (results.affectedRows == 0) res.json({ result: 0 }); 
-			else res.json({ result: 1 });
+			else if (results.affectedRows == 0) {
+				res.json({ 
+					error: "E001",
+					error_message: "해당 공지가 존재하지 않음."
+				})
+			}
+			else getNoticeData(req.body.id, (notice) => res.json(notice));
 		})
 	})
 })
