2ac2b8c5421247406a7be2f52eae9a72d951659c
rudolf0328 <ck0716080@gmail.com>
2022-06-09 15:40:33 +0900

    d

--- routes/user.js
+++ routes/user.js
@@ -24,9 +24,9 @@ router.get('/:id/character', (req, res) => {
   })
 })
 
-router.get('/:id/login', async (req, res) => {
+router.get('/:id/login', (req, res) => {
   app.getConnectionPool((conn) => {
-    const userInfo = await user.getUserInfo('kapi.kakao.com', req.headers.access_token);
+    const userInfo = user.getUserInfo('kapi.kakao.com', req.headers.access_token);
     var sql = "select * from USER where access_token=" + userInfo.id;
     conn.query(sql, function(err, user) {
       conn.release();
