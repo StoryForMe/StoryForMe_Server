7f8808ba438407cd7ec37c286f89199aee7b2e1e
SeoSiun <55352430+SeoSiun@users.noreply.github.com>
2022-06-14 19:31:40 +0900

    88 get series list (#93)
    Keyword의 id를 string으로 변경, content를 없앰.
    
    시리즈 전체 목록 가져오는 api와 키워드별 시리즈 가져오는 api를 분리

--- routes/series.js
+++ routes/series.js
@@ -6,46 +6,22 @@ const user = require('../utils/user');
 const episode = require('../utils/episode');
 const series = require('../utils/series');
 
+router.get('/list/:option/:uid', (req, res) => {
+	app.getConnectionPool((conn) => {
+		conn.query(series.get_series_list_sql(req.params.option, null), function(err, series_list) {
+			conn.release();
+			if(err) console.log(err);
+			series.makeResForSeriesList(series_list, req, res);
+	   })
+	})
+})
+
 router.get('/list/:option/:uid/:kid', (req, res) => {
 	app.getConnectionPool((conn) => {
 		conn.query(series.get_series_list_sql(req.params.option, req.params.kid), function(err, series_list) {
 			conn.release();
-			if(err) console.log("err");
-			else if(!series_list) {
-				console.log("no exist series"); 
-				res.json({ 
-					error: "E001",
-					error_message: "시리즈가 존재하지 않습니다."
-				})
-			}
-			else {
-				results = [];
-				// 각각의 시리즈에 대해 필요한 정보들을 가져와서 results에 추가해줌.
-				function getNicknameIterCallback(nickname, index) {
-					user.getIs_zzimkkong(req.params.uid, series_list[index]["id"], (is_zzimkkong) => {
-						keyword.getSeriesKeyword(series_list[index]["id"], (keywords) => {
-							results.push({
-								sid: series_list[index]["id"],
-								title: series_list[index]["title"],
-								writer: nickname,
-								uid: series_list[index]["uid"],
-								image: series_list["image"],
-								keywords: keywords,
-								hits: series_list[index]["hits"],
-								zzimkkong: series_list[index]["zzimkkong"],
-								episode_num: series_list[index]["episode_num"],
-								is_zzimkkong: is_zzimkkong,
-								is_end: series_list[index]["is_end"],
-								recent_update: series_list[index]["recent_update"]
-							});
-							if (index < series_list.length - 1)
-								user.getNicknameIter(series_list[index + 1]["uid"], index + 1, getNicknameIterCallback)
-							else res.json( {series_list: results });
-						});
-					});
-				}
-				user.getNicknameIter(series_list[0]["uid"], 0, getNicknameIterCallback)
-			}
+			if(err) console.log(err);
+			series.makeResForSeriesList(series_list, req, res);
 	   })
 	})
 })
@@ -83,20 +59,14 @@ router.post('/', (req, res) => {
 			else if (req.body.keywords.length == 0) 
 				series.getSeriesData(results.insertId, (series_data) => res.json(series_data));
 			else {
-				var kid_list = []
-				function getKeywordIdCallback(kid, next_index) {
-					kid_list.push(kid)
-					if (next_index == req.body.keywords.length) 
-						keyword.postSeriesKeyword(results.insertId, kid_list, 0, postSeriesKeywordCallback);
-					else 
-						keyword.getKeywordId(req.body.keywords, next_index, getKeywordIdCallback);
+				var index = 0;
+				function addKeywordToSeriesCallback() {
+					index++;
+					if (index == req.body.keywords.length) 
+						series.getSeriesData(results.insertId, (series_data) => res.json(series_data));
+					else keyword.addKeywordToSeries(results.insertId, req.body.keywords[index], addKeywordToSeriesCallback);
 				}
-				function postSeriesKeywordCallback(next_index) {
-					if (next_index == kid_list.length) 
-						series.getSeriesData(results.insertId, (series_data) => res.json(series_data)); 
-					else keyword.postSeriesKeyword(results.insertId, kid_list, next_index, postSeriesKeywordCallback);
-				}
-				keyword.getKeywordId(req.body.keywords, 0, getKeywordIdCallback);
+				keyword.addKeywordToSeries(results.insertId, req.body.keywords[index], addKeywordToSeriesCallback);
 			}
 		})
 	})
@@ -124,25 +94,30 @@ router.patch('/', (req, res) => {
 					error_message: "해당 시리즈가 존재하지 않음."
 				})
 			}
-			else if (req.body.keywords == null || req.body.keywords.length == 0)
-				keyword.updateSeriesKeyword(req.body.id, null, () => { 
-					series.getSeriesData(req.body.id, (series_data) => res.json(series_data)); 
-				})
+			else if (req.body.keywords == null)
+				series.getSeriesData(req.body.id, (series_data) => res.json(series_data));
 			else {
-				var kid_list = []
-				function getKeywordIdCallback(kid, next_index) {
-					kid_list.push(kid)
-					if (next_index == req.body.keywords.length) 
-						keyword.updateSeriesKeyword(req.body.id, kid_list, postSeriesKeywordCallback);
-					else 
-						keyword.getKeywordId(req.body.keywords, next_index, getKeywordIdCallback);
-				}
-				function postSeriesKeywordCallback(next_index) {
-					if (next_index == kid_list.length) 
-						series.getSeriesData(req.body.sid, (series_data) => res.json(series_data));
-					else keyword.postSeriesKeyword(req.body.sid, kid_list, next_index, postSeriesKeywordCallback);
-				}
-				keyword.getKeywordId(req.body.keywords, 0, getKeywordIdCallback);
+				keyword.getSeriesKeyword(req.body.id, (keyword_list) => {
+					var index = 0;
+					function addKeywordToSeriesCallback() {
+						index++;
+						if (index == req.body.keywords.length) {
+							index = 0;
+							keyword.deleteKeywordFromSeries(req.body.id, keyword_list[index], deleteKeywordFromSeriesCallback);
+						}
+						else if (keyword_list.indexOf(req.body.keywords[index]) != -1) addKeywordToSeriesCallback()
+						else keyword.addKeywordToSeries(req.body.id, req.body.keywords[index], addKeywordToSeriesCallback);
+					}
+					function deleteKeywordFromSeriesCallback() {
+						index++;
+						if (index == keyword_list.length) {
+							series.getSeriesData(req.body.id, (series_data) => res.json(series_data));
+						}
+						else if (req.body.keywords.indexOf(keyword_list[index]) != -1) deleteKeywordFromSeriesCallback()
+						else keyword.deleteKeywordFromSeries(req.body.id, keyword_list[index], deleteKeywordFromSeriesCallback);
+					}
+					keyword.addKeywordToSeries(req.body.id, req.body.keywords[index], addKeywordToSeriesCallback);
+				})
 			}
 		})
 	})
