17a37aefdf90c52358ba660d1e30a0eae3cec685
Rudolf0328 <54528011+Rudolf0328@users.noreply.github.com>
2022-06-14 18:10:16 +0900

    86 user get keywords (#89)
    - user가 like한 keyword들을 보내주는 api
    - keyword들을 string으로 보내줌

--- utils/keyword.js
+++ utils/keyword.js
@@ -73,14 +73,14 @@ exports.getKeywordId = (keywords, index, callback) => {
 // uid애 해당하는 user가 like로 등록한 keyword들을 가져옴
 exports.getUserKeyword = (uid, callback) => {
 	app.getConnectionPool((conn) => {
-		var sql = "select content from KEYWORD as k join `LIKE` as l on k.id=l.kid where uid=" + uid;
+		var sql = "select id from KEYWORD as k join `LIKE` as l on k.id=l.kid where uid=" + uid;
 		conn.query(sql, function(err, rows) {
 			conn.release();
 			if(err) console.log("getUserKeyword " + err);
 			else {
 				var keywords = [];
 				for (var row of rows) {
-					keywords.push(row["content"]);
+					keywords.push(row["id"]);
 				}
 				callback(keywords);
 			}
