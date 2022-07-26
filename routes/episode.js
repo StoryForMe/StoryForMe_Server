182f7fad002891ac424d2386dc634afaad0ac8ee
rudolf0328 <ck0716080@gmail.com>
2022-07-26 23:09:37 +0900

    d

--- routes/episode.js
+++ routes/episode.js
@@ -40,9 +40,8 @@ router.get('/:eid/comment', (req, res) => {
 router.get('/:eid/:uid', (req, res) => {
 	episode.getEpisodeData(res, req.params.eid, req.params.uid, (episode_data) => {
     episode.getEpisodeSid(res, req.params.eid, (sid) => {
-      read.recordReadInfo(res, req.params.uid, sid, req.params.eid, episode_data["chapter"], new Date(), (result) => {
+      read.recordReadInfo(res, req.params.uid, sid, req.params.eid, episode_data["chapter"], new Date().toISOString().replace(/T/, ' ').replace(/\..+/, ''), (result) => {
         res.json(episode_data)
-        // new Date().toISOString().replace(/T/, ' ').replace(/\..+/, '')
       })
     })
   });
