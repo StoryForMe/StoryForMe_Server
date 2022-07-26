39b58cb565513cb71f095a4601c1b77e61e021ec
rudolf0328 <ck0716080@gmail.com>
2022-07-26 22:25:44 +0900

    d

--- utils/read.js
+++ utils/read.js
@@ -1,25 +1,56 @@
 const app = require('../app');
 
-exports.addReadInfo = (res, uid, sid, eid, chapter, date, callback) => {
+exports.recordReadInfo = (res, uid, sid, eid, chapter, date, callback) => {
   app.getConnectionPool((conn) => {
-    var sql = "update `READ` SET ? where uid=" + uid + " and sid=" + sid;
-    var values = {
-      eid: eid,
-      recent_episode: chapter,
-      date: date
-    }
+    var sql = "select * from `READ` where uid=" + uid + " and sid=" + sid;
+    conn.query(sql, values, function(err, results) {
+      conn.release();
+
+      if(err) {
+        addReadInfo(uid, sid, eid, chapter, date);
+      }
+      else {
+        updateReadInfo(uid, sid, eid, chapter, date);
+      }
+    })
 
     conn.query(sql, values, function(err, results) {
       conn.release();
       if(err) {
         res.status(400).json({
           error: "E001",
-          error_message: "존재하지 않는 id가 있음"
+          error_message: err
         })
       }
       else {
         callback({ result: 1 })
       }
     })
+  })
+}
+
+let updateReadInfo = (uid, sid, eid, chapter, date) => {
+  var sql = "update `READ` SET ? where uid=" + uid + " and sid=" + sid;
+  var values = {
+    eid: eid,
+    recent_episode: chapter,
+    date: date
+  }
+  conn.query(sql, values, function(err, results) {
+    conn.release();
+  })
+}
+
+let addReadInfo = (uid, sid, eid, chapter, date) => {
+  var sql = "insert into `READ` SET ? ";
+  var values = {
+    uid: uid,
+    eid: eid,
+    sid: sid,
+    recent_episode: chapter,
+    date: date
+  }
+  conn.query(sql, values, function(err, results) {
+    conn.release();
   })
 }
