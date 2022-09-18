ef3ba3f00a60246a1b6545afbfdb5b27d7509109
SeoSiun <siun0816@hanyang.ac.kr>
2022-09-18 18:31:08 +0900

    .

--- models/KeywordModel.js
+++ models/KeywordModel.js
@@ -9,6 +9,8 @@ exports.findAll = (uid) => {
     app.getConnectionPool(async (conn) => {
         var sql = "select * from KEYWORD as k join `LIKE` as l on k.id=l.kid where uid=" + uid;
         [err, results] = await conn.query(sql);
+        console.log(err)
+        console.log(results)
         conn.release();
         if (err) {
             return {
