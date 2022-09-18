61043fe81e73ae0984bcc2018fd0c8f302c7ea94
SeoSiun <siun0816@hanyang.ac.kr>
2022-09-18 18:29:12 +0900

    .

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -7,30 +7,31 @@ const KeywordModel = require('../models/KeywordModel')
 
 exports.getUser = (req, res) => {
     let getUserResponse = UserModel.findOne(req.params.id);
-    if (getUserResponse.code === UserModel.UserModelCodes.USER_NOT_FOUND) {
+    if (getUserResponse['code'] === UserModel.UserModelCodes.USER_NOT_FOUND) {
         res.status(HTTP.StatusCodes.NOT_FOUND).json({
             error: "E001",
-            error_message: getUserResponse.code,
+            error_message: getUserResponse['code'],
         })
     }
-    if (getUserResponse.code !== UserModel.UserModelCodes.SUCCESS) {
+    if (getUserResponse['code'] !== UserModel.UserModelCodes.SUCCESS) {
         res.status(HTTP.StatusCodes.BAD_REQUEST).json({
             error: "E002",
-            error_message: getUserResponse.code,
+            error_message: getUserResponse['code'],
         })
     }
 
     let getKeywordResponse = KeywordModel.findAll(req.params.id);
-    if (getKeywordResponse.code !== KeywordModel.KeywordModelCodes.SUCCESS) {
+    if (getKeywordResponse['code'] !== KeywordModel.KeywordModelCodes.SUCCESS) {
         res.status(HTTP.StatusCodes.BAD_REQUEST).json({
             error: "E002",
-            error_message: getKeywordResponse.code,
+            error_message: getKeywordResponse['code'],
         })
     }
 
-    let keywords = _.map(getKeywordResponse.keywords, (keyword) => ({
+    let keywords = _.map(getKeywordResponse['keywords'], (keyword) => ({
         id: keyword['id'],
     }));
+    const user = getUserResponse['user'];
     res.status(HTTP.StatusCodes.OK).json({
         nickname: user["nickname"],
         introduction: user["introduction"],
